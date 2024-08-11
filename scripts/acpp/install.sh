2c5d51bd13e9cfd7c9ae3d39bf2560433230e98c
Jack Myers <jackhmyers97@gmail.com>
2024-08-10 23:05:17 -0700

    minor fixes

--- scripts/acpp/install.sh
+++ scripts/acpp/install.sh
@@ -8,6 +8,7 @@ cd "$PIXI_PROJECT_ROOT"/build
 cmake --install .
 mkdir -p /etc/project-manifests/acpp-project
 cp /build/acpp/pixi.toml /etc/project-manifests/acpp-project/pixi.toml
+cat /build/acpp/scripts/pixi.toml.append | tee -a /etc/project-manifests/acpp-project/pixi.toml
 
 cd /
 rm -rf build/acpp
